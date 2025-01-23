96d2d61499fc8ce44ef5d76ea67bae2fe3f018fd
lajixiaohao <2176974730@qq.com>
2025-01-23 17:28:51 +0800

    去掉资源，简化程序

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() {
   let config = slint_build::CompilerConfiguration::new().with_style("cosmic".into());
   slint_build::compile_with_config("ui/app-window.slint", config).unwrap();
-
-  if cfg!(target_os = "windows") {
-    let mut res = winresource::WindowsResource::new();
-    res.set_icon("ui/assets/32x32.ico").compile().unwrap();
-  }
 }
